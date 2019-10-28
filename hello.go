d6b2e333923f5819566b4c52be355db9fc43cc73
kino <kinoflee@gmail.com>
2019-10-28 09:18:12 +0100

    basic data structures

--- hello.go
+++ hello.go
@@ -6,7 +6,37 @@ import (
 )
 
 func main() {
-	// const has no type until given one
 	const msg = "Hello earth! \n"
+
+	// [...]T syntax is sugar for [n]T
+	// It creates a fixed size array, but lets the compiler figure out how many elements are in it.
+	// There's no such thing as constant array in go
+
+	// here the msgs are an array (which will be [2]string for compiler)
+	msgs := [...]string{"How are you going? \n", "We're in trouble \n"}
+
+	// But here the msgs1 are a slice 🌚👻
+	// the only difference is the `...` inside `[]`
+	msgs1 := []string{"How are you going? \n", "We're in trouble \n"}
+
+	/*
+		A few differences between array/slice in go:
+		slice is built on array.
+		- the length of an array is part of its type ([4]int and [5]int is not compatible)
+		so the type of array is always [n]T, but the type of a slice is []T
+		- Zero value for int array is 0, whereas zero value for int slice is nil
+	*/
+
+	msgs2 := map[int]string{1: "How are you going? \n", 2: "We're in trouble \n"}
+
 	fmt.Println(test.Response(msg))
+	fmt.Println(test.Response(msgs))
+	fmt.Println(test.Response(msgs1))
+	fmt.Println(test.Response(msgs2))
+
+	// for loop is the only way to iterate sth in go
+	// https://stackoverflow.com/questions/48693172/how-do-i-iterate-over-an-array-of-a-custom-type-in-go
+	for _, msg := range msgs {
+		fmt.Printf(test.Response(msg))
+	}
 }
--- test/test.go
+++ test/test.go
@@ -1,11 +1,15 @@
 package test
 
 import (
+	"reflect"
 	"strconv"
 	"time"
 )
 
-func Response(msg string) string {
+// How to declare union type in go ??
+
+// anything started with uppercase letter in a package is automatically exported
+func Response(msg interface{}) string {
 	/* For Variables
 	Option 1:
 	var currentTime int64
@@ -18,6 +22,47 @@ func Response(msg string) string {
 	currentTime := time.Now().Unix()
 	*/
 	currentTime := strconv.FormatInt(time.Now().Unix(), 10)
+
+	// print the memory address (via using `&`) for fun
+	// also note there's a `*` to use pointer
+	p := &currentTime
+	println(p)
+
+	// const has no type until given one
+	// and the value must be decided in compile time
 	const res = "hello alien "
-	return msg + res + currentTime
+
+	// function def inside function: assign the func to a local variable
+	handleNonPrimitive := func(val interface{}) string {
+		// as type switch only work for specific type, for array/slice/map, you need to use reflect
+		// it's like the metadata of var
+		v := reflect.ValueOf(val)
+		switch v.Kind() {
+		case reflect.Slice:
+			// how to check the element type & value inside an slice ?
+			return "weird thing here we got a slice"
+		case reflect.Array:
+			return "An array!!"
+		case reflect.Map:
+			return "The alien passing a map"
+		default:
+			return "nothing here"
+		}
+	}
+
+	// use interface for type switch
+	// see https://tour.golang.org/methods/16
+	// note the `.(type)` here
+	switch msgType := msg.(type) {
+	case string:
+		return msgType + res + currentTime
+	case int64:
+		return strconv.FormatInt(msgType, 10) + res + currentTime
+	default:
+		return handleNonPrimitive(msgType)
+	}
+}
+
+func handleNonPrimitive(val interface{}) string {
+	return "nothing outside"
 }
